d815bad57de22152876ee1b945f6c5e9545c9471
Epsi22-git <meritorioustime@gmail.com>
2025-02-04 18:13:37 +0530

    contact.js

---
+++ models/contact.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phoneNumber: { type: String, required: true },
+    address: { type: String },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
